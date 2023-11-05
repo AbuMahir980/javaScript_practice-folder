d8bdf48d222c1e15a5628c9cbda32c897429625d
Qudus Lawal <lawalqudus980@gmail.com>
2023-11-05 22:56:41 +0100

    Update code base

--- odinProjectChallenges.js
+++ odinProjectChallenges.js
@@ -36,6 +36,7 @@ const repeatString = function (string, num) {
 };
 // console.log(repeatString("hey", 20));
 
+
 /*
 2 - Reverse a String
 
@@ -51,6 +52,8 @@ const reverseString = function (stringVal) {
 };
 // console.log(reverseString("hello, AbdulQudus!"));
 
+
+
 /*
 3 - removeFromArray
 
@@ -65,4 +68,43 @@ const removeFromArray = function (arr, ...args) {
 };
 // console.log(removeFromArray([1, 2, 3, 4], 3));
 
+
+
+/**
+ * 4 - sumAll
+ * Implement a function that takes 2 integers and returns the sum of every number between(and including) them:
+ * sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
+ * Hints
+Think about how you would do this on pen and paper and then how you might translate that process into code:
+
+make sure you pay attention to the function parameters
+create a variable to hold the final sum
+loop through the given numbers (link)
+on each iteration add the number to the sum
+return the sum after finishing the loop
+ */
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    let sum = 0
+
+    if (num1 < num2){
+        for (let i = num1; i <= num2; i++) {
+					sum += i;
+					// console.log(sum)
+				}
+
+    } else {
+        for (let i = num2; i <= num1; i++) {
+					sum += i;
+					// console.log(sum)
+				}
+    }
+    
+    return sum;
+};
+// console.log(sumAll(1, 4));
+// console.log(sumAll(1, 4000));
+// console.log(sumAll(10, [90, 1]));
+console.log(sumAll(123, 1));
+// console.log(sumAll(10, "90"));
 
