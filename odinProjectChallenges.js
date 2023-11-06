a163dcc190fb5cb0f18c945b936afee84cfaf70c
Qudus Lawal <lawalqudus980@gmail.com>
2023-11-06 09:13:30 +0100

    Update changes

--- odinProjectChallenges.js
+++ odinProjectChallenges.js
@@ -60,7 +60,13 @@ const reverseString = function (stringVal) {
 Implement a function that takes an array and some other arguments then removes the other arguments from that array:
 
 ```javascript
-removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
+removeFromArray([1, 2, 3, 4], 3); // output: [1,2,4]
+removeFromArray([1, 2, 3, 4], 3, 2));//output: [1, 4]
+removeFromArray([1, 2, 3, 4], 7, "tacos"));//output: [1, 2, 3, 4]
+removeFromArray([1, 2, 3, 4], 7, 2));//output: [1, 3, 4]
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4));//output: []
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3));//output: [2, "ho"]
+removeFromArray([1, 2, 3], "1", 3));//output: [1, 2]
 ```
 */
 const removeFromArray = function (arr, ...args) {
